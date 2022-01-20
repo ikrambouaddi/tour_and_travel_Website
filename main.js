02025e7a6eba721fa3ba5b0a0ae9f8809dbb4606
unknown <ikrambouaddi11@gmail.com>
2022-01-20 22:37:19 +0100

    add services galolery review

--- main.js
+++ main.js
@@ -39,11 +39,3 @@ videsbtn.forEach( btn =>{
     });
 });
 
-// videoBtn.forEach(btn =>{
-//     btn.addEventListener('click', ()=>{
-//         document.querySelector('.controls .active').classList.remove('active');
-//         btn.classList.add('active');
-//         let src = btn.getAttribute('data-src');
-//         document.querySelector('#video-slider').src = src;
-//     });
-// });
