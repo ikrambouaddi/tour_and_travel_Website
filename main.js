f42933778b98c83cd6c3682bd52735f4525ba359
unknown <ikrambouaddi11@gmail.com>
2022-01-21 21:29:14 +0100

    style footer

--- main.js
+++ main.js
@@ -39,3 +39,66 @@ videsbtn.forEach( btn =>{
     });
 });
 
+// var swiper = new Swiper (".review-slider",{
+//     spaceBetween: 20,
+//     loop:true,
+//     autoplay:{
+//         delay:2500, 
+//         disableOnInteraction: false,
+//     },
+//     breakpoints: {
+//         640: {
+//             slidesPerview: 1,
+//         },
+//         768: {
+//             slidesPerview: 2,
+//         },
+//         1024: {
+//             slidesPerview:3,
+//         },
+//     },
+// });
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+        slidesPerView: 1,
+        },
+        768: {
+        slidesPerView: 2,
+        },
+        1024: {
+        slidesPerView: 3,
+        },
+    },
+});
+
+
+var swiper = new Swiper(".brand-slider", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        450: {
+        slidesPerView: 2,
+        },
+        768: {
+        slidesPerView: 3,
+        },
+        991: {
+        slidesPerView: 4,
+        },
+        1200: {
+            slidesPerView: 5,
+            },
+    },
+});
